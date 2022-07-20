3ed93eff643bedd6461cdbcc6c0a7047a9bd16ea
Хетаг Хачиров <kh.ha4irov2011@yandex.ru>
2022-07-20 14:17:31 +0300

    Добавьте файлы проекта.

---
+++ ModelUsualFractionWorkConsoleApp/Fraction.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace ModelUsualFractionWorkConsoleApp
+{
+    public class Fraction
+    {
+        public int Numerator;
+        public int Denominator;
+
+        public Fraction(int numerator, int denominator)
+        {
+            Numerator = numerator;
+            Denominator = denominator;
+        }
+
+        public Fraction(int numerator)
+        {
+            Numerator = numerator;
+            Denominator = 1;
+        }
+
+        public void Print()
+        {
+            if (Denominator == 1)
+            {
+                Console.WriteLine($"{Numerator}");
+            }
+            else
+            {
+                Console.WriteLine($"{Numerator}/{Denominator}");
+            }
+        }
+
+        public void Cut()
+        {
+            if (Numerator % Denominator == 0)
+            {
+                Numerator /= Denominator;
+            }
+            else if (Denominator % Numerator == 0)
+            {
+                Numerator /= Numerator;
+                Denominator /= Numerator;
+            }
+            else
+            {
+                for (int i = Math.Max(Numerator, Denominator); i > 0; i--)
+                {
+                    if(Numerator % i == 0 && Denominator % i == 0)
+                    {
+                        Numerator /= i;
+                        Denominator /= i;
+                        break;
+                    }
+                }
+            }
+        }
+
+        public Fraction Sum(Fraction otherFraction)
+        {
+            var commonDominator = Denominator * otherFraction.Denominator;
+            var resultNumerator = Numerator * otherFraction.Denominator + otherFraction.Numerator * Denominator;
+            var result = new Fraction(resultNumerator, commonDominator);
+            result.Cut();
+            return result;
+        }
+
+        public Fraction Difference(Fraction otherFraction)
+        {
+            var commonDominator = Denominator * otherFraction.Denominator;
+            var resultNumerator = Numerator * otherFraction.Denominator - otherFraction.Numerator * Denominator;
+            var result = new Fraction(resultNumerator, commonDominator);
+            result.Cut();
+            return result;
+        }
+
+        public Fraction Multiplication(Fraction otherFraction)
+        {
+            var result = new Fraction(Numerator * otherFraction.Numerator, Denominator * otherFraction.Denominator);
+            result.Cut();
+            return result;
+        }
+
+        public Fraction Divide(Fraction otherFraction)
+        {
+            var result = new Fraction(Numerator * otherFraction.Denominator, Denominator * otherFraction.Numerator);
+            result.Cut();
+            return result;
+        }
+
+        public Fraction Sum(int number)
+        {
+            var otherFraction = new Fraction(number);
+            return Sum(otherFraction);
+        }
+
+        public Fraction Difference(int number)
+        {
+            var otherFraction = new Fraction(number);
+            return Difference(otherFraction);
+        }
+
+        public Fraction Multiplication(int number)
+        {
+            var otherFraction = new Fraction(number);
+            return Multiplication(otherFraction);
+        }
+
+        public Fraction Divide(int number)
+        {
+            var otherFraction = new Fraction(number);
+            return Divide(otherFraction);
+        }
+    }
+}
---
+++ ModelUsualFractionWorkConsoleApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelUsualFractionWorkConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fraction1 = new Fraction(2, 2);
+            fraction1.Print();
+            var fraction2 = new Fraction(2, 2);
+            fraction2.Print();
+
+            var result = fraction1.Sum(fraction2);
+            result.Print();
+
+            result = fraction1.Difference(fraction2);
+            result.Print();
+
+            result = fraction1.Multiplication(fraction2);
+            result.Print();
+
+            result = fraction1.Divide(fraction2);
+            result.Print();
+
+            Console.WriteLine("------------------------");
+
+            result = fraction1.Sum(1);
+            result.Print();
+
+            result = fraction1.Difference(1);
+            result.Print();
+
+            result = fraction1.Multiplication(2);
+            result.Print();
+
+            result = fraction1.Divide(2);
+            result.Print();
+        }
+    }
+}
